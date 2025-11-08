d3eb27f55cf186623ee01497993174ded7d80102
OlexandrProkopchuk <sprok2005@gmail.com>
2025-11-08 21:08:36 +0200

    Add Quiz entity

---
+++ EduPlatformSolution/Data/Quiz.cs
@@ -0,0 +1,25 @@
+﻿using EduPlatform.Data;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace EduPlatform.Data
+{
+    public class Quiz
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public int CourseId { get; set; }
+        public Course? Course { get; set; }
+
+        [Required, StringLength(200)]
+        public string Title { get; set; } = string.Empty;
+
+        // ліміт часу (хвилини), 0 = без ліміту
+        [Range(0, 600)]
+        public int TimeLimitMinutes { get; set; } = 0;
+
+        public ICollection<Question> Questions { get; set; } = new List<Question>();
+        public ICollection<Attempt> Attempts { get; set; } = new List<Attempt>();
+    }
+}
