ac78c68532badd508c01fcdcfa304677a9b8543b
OlexandrProkopchuk <sprok2005@gmail.com>
2025-11-10 01:07:50 +0200

    Implemented quiz taking feature and answer grading.

---
+++ EduPlatformSolution/Services/IQuizService.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using EduPlatform.Data;
+
+namespace EduPlatform.Services;
+
+public interface IQuizService
+{
+    Task<Attempt> GradeAsync(int quizId, string userId, Dictionary<int, int[]> answers);
+}
